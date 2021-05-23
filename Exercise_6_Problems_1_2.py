aaa2337d7b0d4f188deb1595ef709465f134046c
HIRO1129 <>
2021-05-23 13:04:28 +0000

    I reading the data file data/1091402.txt into a variable called data.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -17,6 +17,9 @@ import numpy as np
 data = None
 
 # YOUR CODE HERE 1
+fp = "data/1091402.txt"
+data = pd.read_csv(fp, delim_whitespace = True, skiprows = [1], na_values = [-9999])
+
 
 # ### Part 2 
 # 
@@ -27,7 +30,7 @@ data = None
 
 tavg_nodata_count = None
 #YOUR CODE HERE 2
-
+tavg_nodata_count = data.iloc[:,6].isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -40,6 +43,7 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 
 tmin_nodata_count = None
 #YOUR CODE HERE 3
+tmin_nodata_count =  data.iloc[:,8].isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -52,6 +56,8 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
+day_count = len(data)
+
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -65,6 +71,7 @@ print("Number of days:", day_count)
 first_obs = None
  
 # YOUR CODE HERE 5
+first_obs = data.iloc[0,4]
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -77,6 +84,8 @@ print('Date of the first observation:',first_obs)
 last_obs = None
 
 # YOUR CODE HERE 6
+last_obs = data.iloc[day_count-1, 4]
+
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -90,6 +99,8 @@ print('Date of the last observation:', last_obs)
 avg_temp = None
 
 # YOUR CODE HERE 7
+avg_temp = np.mean(data.iloc[:, 6])
+
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -103,6 +114,9 @@ print('Average temperature (F) for the whole dataset:', round(avg_temp, 2))
 avg_temp_1969 = None
 
 # YOUR CODE HERE 8
+data_1969summer = data.query('19690501 <= DATE <= 19690831')
+avg_temp_1969 = np.mean(data_1969summer.iloc[:,7])
+
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print a number
@@ -116,6 +130,9 @@ print('Average temperature (F) for the Summer of 69:', round(avg_temp_1969, 2))
 monthly_data = None
 
 # YOUR CODE HERE 9
+data['MONTH'] = data['DATE'].str.slice(start = 0, stop = 6)
+monthly_data = pd.DataFrame({'month':1, 'temp_celsius':1})
+
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print the length of variable monthly_data
